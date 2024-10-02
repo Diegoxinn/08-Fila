7b385e34694a09704cda55eeaa28a6778071ccc2
Diegoxin <diego07_ferreira@hotmail.com>
2024-10-01 23:05:19 -0300

    Atividade realizada.

--- Fila/Fila/Fila.cpp
+++ Fila/Fila/Fila.cpp
@@ -88,13 +88,29 @@ void insere()
 	cin >> novo->valor;
 	novo->prox = NULL;
 
-
+	if (inicio == NULL) {
+		inicio = novo;
+		fim = novo;
+	}
+	else {
+		fim->prox = novo;
+		fim = novo;
+		cout << fim->valor << " ultimo" << endl;
+		cout << inicio->valor << " primeiro" << endl;
+	}
 }
 
 void remove()
 {
+	NO* aux = inicio;
 
-
-
+	if (inicio == NULL) {
+		cout << "Lista vazia.";
+	}
+	else {
+		inicio = inicio->prox;
+		cout << "Elemento apagado: " << aux->valor << endl;
+		free(aux);
+	}
 }
 
